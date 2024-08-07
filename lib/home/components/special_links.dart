33a868b9d9121c5f54727210d348aa4d555de980
Igiz <igizstarkle@gmail.com>
2024-08-07 09:46:54 +0300

    Added Section Tile

---
+++ lib/home/components/special_links.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SpecialLinks extends StatelessWidget {
+  const SpecialLinks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+        ),
+      ],
+    );
+  }
+}
